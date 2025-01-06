42c27ed5a7efa2d992d54f68633111725c896fd6
Maksim Buga <maksim.booga@gmail.com>
2025-01-06 19:41:18 +0400

    Revert "feat: add solution"
    This reverts commit 923ae5f461742c8005d12b2c9af109912ad31fb5.

--- src/what-season.js
+++ src/what-season.js
@@ -14,7 +14,6 @@ const { NotImplementedError } = require('../extensions/index.js');
 function getSeason(/* date */) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-  }
 }
 
 module.exports = {
