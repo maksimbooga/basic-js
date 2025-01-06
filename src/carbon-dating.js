33cb8657436b889157ea4bde5c47f82668deea67
Maksim Buga <maksim.booga@gmail.com>
2025-01-06 20:11:03 +0400

    feat: add solution
    dateSample

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,8 +17,11 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  if ( typeof sampleActivity === 'string' && sampleActivity > 0 && sampleActivity <= MODERN_ACTIVITY ) {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) * HALF_LIFE_PERIOD / Math.LN2);
+  }
+  return false;
   // remove line with error and write your code here
 }
 
